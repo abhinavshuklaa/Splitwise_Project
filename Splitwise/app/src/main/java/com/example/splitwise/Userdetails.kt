7295c9e949ed1d6e10fffd692c3071c2d54495f1
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2020-11-24 01:11:43 +0530

    #2 SignUP Activity and UserDetails Saving into Firebase RealTimedatabse

---
+++ Splitwise/app/src/main/java/com/example/splitwise/Userdetails.kt
@@ -0,0 +1,10 @@
+package com.example.splitwise
+
+data class Userdetails(
+    val userId: String? = null,
+    val email: String? = null,
+    val mpwd: String? = null,
+    val musername: String? = null,
+    val imageurl: String? = null
+
+)
