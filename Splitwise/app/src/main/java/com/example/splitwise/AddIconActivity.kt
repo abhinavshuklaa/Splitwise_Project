94083b7f868bea0a72ed57d5bddb2716494aefc9
Abhinav Shukla <abhinav2014shukla@gmail.com>
2020-11-23 23:27:23 +0530

    Created Activity to add a friend and its xml

---
+++ Splitwise/app/src/main/java/com/example/splitwise/AddIconActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splitwise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddIconActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_icon)
+    }
+}
--- Splitwise/app/src/main/java/com/example/splitwise/Splitwise_HomeActivity.kt
+++ Splitwise/app/src/main/java/com/example/splitwise/Splitwise_HomeActivity.kt
@@ -1,11 +1,39 @@
 package com.example.splitwise
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentPagerAdapter
+import kotlinx.android.synthetic.main.activity_splitwise__home.*
+
 
 class Splitwise_HomeActivity : AppCompatActivity() {
+private lateinit var fregmentAdapter_Splitwise_HomeActivity: FragmentAdapter_Splitwise_HomeActivity
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splitwise__home)
+        setViewPagerAdapter()
+        InitializingAndsettingViewPager()
+
+
     }
+
+    private fun InitializingAndsettingViewPager() {
+        viewPagerFragmentFriends.adapter=fregmentAdapter_Splitwise_HomeActivity
+        tabLayoutFragmentFriends.setupWithViewPager(viewPagerFragmentFriends)
+
+
+    }
+
+    private fun setViewPagerAdapter() {
+        fregmentAdapter_Splitwise_HomeActivity = FragmentAdapter_Splitwise_HomeActivity(
+            supportFragmentManager,
+            FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+
+        )
+
+    }
+
 }
