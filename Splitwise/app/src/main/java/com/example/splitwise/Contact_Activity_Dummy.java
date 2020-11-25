6b1053ecf64f7e0c79b5b5607a2a4eec98c8dae6
Abhinav Shukla <abhinav2014shukla@gmail.com>
2020-11-25 10:22:58 +0530

    Merging with master#1

---
+++ Splitwise/app/src/main/java/com/example/splitwise/Contact_Activity_Dummy.java
@@ -0,0 +1,123 @@
+package com.example.splitwise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Contact_Activity_Dummy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact___dummy);
+    }
+    public class Android_Contact {
+        public String android_contact_Name = "";
+        public String android_contact_TelefonNr = "";
+        public int android_contact_ID=0;
+    }
+
+    public void fp_get_Android_Contacts() {
+        ArrayList<Android_Contact> arrayList_Android_Contacts = new ArrayList<Android_Contact>();
+
+        Cursor cursor_Android_Contacts = null;
+        ContentResolver contentResolver = getContentResolver();
+        try {
+            cursor_Android_Contacts = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        } catch (Exception ex) {
+            Log.e("Error on contact", ex.getMessage());
+        }
+
+        if (cursor_Android_Contacts.getCount() > 0) {
+            while (cursor_Android_Contacts.moveToNext()) {
+                Android_Contact android_contact = new Android_Contact();
+                String contact_id = cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts._ID));
+                String contact_display_name = cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                android_contact.android_contact_Name = contact_display_name;
+
+
+                int hasPhoneNumber = Integer.parseInt(cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+
+                    Cursor phoneCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                            , null
+                            , ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?"
+                            , new String[]{contact_id}
+                            , null);
+
+                    while (phoneCursor.moveToNext()) {
+                        String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        android_contact.android_contact_TelefonNr = phoneNumber;
+
+                    }
+                    phoneCursor.close();
+                }
+
+                arrayList_Android_Contacts.add(android_contact);
+            }
+
+//            Adapter_for_Android_Contacts adapter = new Adapter_for_Android_Contacts(this, arrayList_Android_Contacts);
+//            listview_Android_Contacts.setAdapter(adapter);
+
+
+        }
+
+    }
+
+    public class Adapter_for_Android_Contacts extends BaseAdapter {
+
+        Context mContext;
+        List<Android_Contact> mList_Android_Contacts;
+
+        public Adapter_for_Android_Contacts(Context mContext, List<Android_Contact> mContact) {
+            this.mContext = mContext;
+            this.mList_Android_Contacts = mContact;
+        }
+//</ constructor with ListArray >
+
+        @Override
+        public int getCount() {
+            return mList_Android_Contacts.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mList_Android_Contacts.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view=View.inflate(mContext,R.layout.item_layout_contact_list,null);
+            TextView textview_contact_Name= (TextView) view.findViewById(R.id.textview_android_contact_name);
+            TextView textview_contact_TelefonNr= (TextView) view.findViewById(R.id.textview_android_contact_phoneNr);
+
+            textview_contact_Name.setText(mList_Android_Contacts.get(position).android_contact_Name);
+            textview_contact_TelefonNr.setText(mList_Android_Contacts.get(position).android_contact_TelefonNr);
+
+
+            view.setTag(mList_Android_Contacts.get(position).android_contact_Name);
+            return view;
+        }
+
+    }
+
+}
--- Splitwise/app/src/main/java/com/example/splitwise/ContactsListActivity.kt
+++ Splitwise/app/src/main/java/com/example/splitwise/ContactsListActivity.kt
@@ -1,44 +1,95 @@
 package com.example.splitwise
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
 import android.os.Bundle
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.*
-import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_contacts_list.*
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ContactsListActivity : AppCompatActivity() {
-    private val databaseReference = FirebaseDatabase.getInstance().getReference("Users")
-    private  var dataList= mutableListOf<Database>()
+    open class Android_Contact{
+        var android_contact_Name = ""
+        var android_contact_TelefonNr = ""
+        var android_contact_ID = 0
+    }
+
+
+//    private val databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+//    private  var dataList= mutableListOf<Database>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts_list)
-        addFriendToDatabase.setOnClickListener {
-            databaseReference.addValueEventListener(object :ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    dataList.clear()
-                    for(i in snapshot.children){
-                        val data:Database=i.getValue(Database::class.java)!!
-                        if(data.userId!= Firebase.auth.currentUser?.uid){
-                            dataList.add(data)
-                        }
-                    }
-                    setAdapter(dataList)
-
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-
-            })
-        }
+//        addFriendToDatabase.setOnClickListener {
+//            databaseReference.addValueEventListener(object :ValueEventListener{
+//                override fun onDataChange(snapshot: DataSnapshot) {
+//                    dataList.clear()
+//                    for(i in snapshot.children){
+//                        val data:Database=i.getValue(Database::class.java)!!
+//                        if(data.userId!= Firebase.auth.currentUser?.uid){
+//                            dataList.add(data)
+//                        }
+//                    }
+//                    setAdapter(dataList)
+//
+//                }
+//
+//                override fun onCancelled(error: DatabaseError) {
+//                    TODO("Not yet implemented")
+//                }
+//
+//            })
+//        }
     }
+
+//    fun fp_get_Android_contact(){
+//        val arrayList_Android_Contacts = ArrayList<Android_Contact>()
+//        var cursor_Android_Contacts: Cursor? = null
+//        val contentResolver = contentResolver
+//        try {
+//            cursor_Android_Contacts =
+//                contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null)
+//        } catch (ex: Exception) {
+//            ex.message?.let { Log.e("Error on contact", it) }
+//        }
+//
+//        if (cursor_Android_Contacts != null) {
+//            if (cursor_Android_Contacts.getCount() > 0) {
+//                while (cursor_Android_Contacts.moveToNext()){
+//                    val android_contact = Android_Contact()
+//                    val contact_id = cursor_Android_Contacts.getString(
+//                        cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts._ID)
+//                    )
+//                    val contact_display_name = cursor_Android_Contacts.getString(
+//                        cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+//
+//                    )
+//                    android_contact.android_contact_Name = contact_display_name;
+//
+//
+//                   var  hasPhoneNumber = Integer.parseInt(cursor_Android_Contacts.getString(cursor_Android_Contacts.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)));
+//                    if (hasPhoneNumber > 0) {
+//
+//                      var phoneCursor= contentResolver.query(
+//                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+//                        , null
+//                        , ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?"
+//                        ,}
+//                        , null);
+//
+//                }
+//
+//
+//            }
+//            }
+//        }
+
 
     private fun setAdapter(dataList: List<Database>) {
 
-
-
     }
+
+
+
 }
