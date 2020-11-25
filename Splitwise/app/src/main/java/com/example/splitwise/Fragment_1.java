cd44a9b4af1c70d3d78574f1167229abcdff4a50
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2020-11-25 10:14:15 +0530

    # Slide Activity along with image Picking from Gallery

--- Splitwise/app/src/main/java/com/example/splitwise/Fragment_1.java
+++ Splitwise/app/src/main/java/com/example/splitwise/Fragment_1.java
@@ -33,7 +33,6 @@ public class Fragment_1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_1, container, false);
     }
 }
